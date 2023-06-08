1d475db71ada6fc80741665dd3e8f18a7109a0c9
Pedro Porsch Spainol <pedroporschspaniol@gmail.com>
2023-06-08 17:33:27 -0300

    Modal adicionado

--- script/modal.js
+++ script/modal.js
@@ -1,6 +1,31 @@
 export default function modal(){
 
-const modal = document.querySelector('.modal');
+const modal = document.querySelector('.modal-bg');
+const botaoLogin = document.querySelector('.login');
+const botaoFechar = document.querySelector('.botao-fechar');
+
+if(modal && botaoLogin && botaoFechar ){
+
+    function mostrarModal(e){
+
+        e.preventDefault();
+        modal.classList.toggle('active');
+    
+    }
+    
+    function fecharModal(e){
+    
+        if(e.target === modal){
+            modal.classList.toggle('active')
+        }
+    
+    }
+
+    botaoFechar.addEventListener('click', mostrarModal)
+    botaoLogin.addEventListener('click', mostrarModal);
+    modal.addEventListener('click', fecharModal);
+}
+
 
 
 
